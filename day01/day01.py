0706e1c6f445fdfc2e86674af00e6c183019f313
Neehi <nick.snape@hotmail.com>
2021-12-02 00:04:18 +0000

    Refactor day 1 solution

--- day01/day01.py
+++ day01/day01.py
@@ -3,11 +3,14 @@ import os
 with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as file:
   measurements = [int(line.strip()) for line in file]
 
+def count_increases(data):
+  return sum(a < b for a, b in zip(data, data[1:]))
+
 def part_one():
-  return sum(a < b for a, b in zip(measurements, measurements[1:]))
+  return count_increases(measurements)
 
 def part_two():
-  return sum(sum(measurements[i:i+3]) < sum(measurements[i+1:i+4]) for i in range(len(measurements) - 3))
+  return count_increases([sum(three_nums) for three_nums in zip(measurements, measurements[1:], measurements[2:])])
 
 if __name__ == "__main__":
   print('Part One: %d' % part_one())
