d12dd06bc305be12802a07cf2c0c2d6da2f28e72
Neehi <nick.snape@hotmail.com>
2021-12-19 11:05:19 +0000

    Refactor day 16 part 1 solution

--- day16/day16.py
+++ day16/day16.py
@@ -5,42 +5,37 @@ with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as file:
 
 transmission = bin(int(data, 16))[2:].zfill(len(data) * 4)
 
-def process_number(packet, num_bits):
-  return int(packet[:num_bits], base=2), packet[num_bits:]
-
-def process_literal(packet):
-  literal = ""
-  while True:
-    prefix = packet[0]
-    literal += packet[1:5]
-    packet = packet[5:]
-    if prefix == "0":
-      break
-  return int(literal, base=2), packet
-
 def process_packet(packet):
   # Version
-  V, packet = process_number(packet, 3)
-  version_sum = V
+  V, packet = int(packet[:3], base=2), packet[3:]
 
   # Type
-  T, packet = process_number(packet, 3)
+  T, packet = int(packet[:3], base=2), packet[3:]
 
   # Literal
   if T == 4:
-    _, packet = process_literal(packet)  # Can be ignored?
+    literal = ""
+    while True:
+      prefix = packet[0]
+      literal += packet[1:5]
+      packet = packet[5:]
+      if prefix == "0":
+        break
+    return V, packet
 
   # Subpackets
-  elif packet[0] == "0":
-    len_subpackets, packet = process_number(packet[1:], 15)
+  version_sum = V
+
+  if packet[0] == "0":  # Packet of size n
+    len_subpackets, packet = int(packet[1:16], base=2), packet[16:]
     to_process = packet[:len_subpackets]
     while len(to_process):
       v, to_process = process_packet(to_process)
       version_sum += v
     packet = packet[len_subpackets:]
 
-  else:
-    num_subpackets, packet = process_number(packet[1:], 11)
+  else:  # n packets
+    num_subpackets, packet = int(packet[1:12], base=2), packet[12:]
     for _ in range(num_subpackets):
       v, packet = process_packet(packet)
       version_sum += v
